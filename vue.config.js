93c7ad4fdf38ceed494a05465055b3e4b59a8cea
cynthiarchang123 <cynthiarchang@gmail.com>
2019-10-28 10:57:09 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,17 @@
-const path = require('path')
+// const path = require('path')
 
+// module.exports = {
+//   chainWebpack: config => {
+//     const apiClient = process.env.VUE_APP_API_CLIENT // mock or server
+//     config.resolve.alias.set(
+//       'api-client',
+//       path.resolve(__dirname, `src/api/${apiClient}`)
+//     );
+//   }
+//   // publicPath: process.env.NODE_ENV === 'production' ? '/my-project/' : '/'
+// }
 module.exports = {
-  chainWebpack: config => {
-    const apiClient = process.env.VUE_APP_API_CLIENT // mock or server
-    config.resolve.alias.set(
-      'api-client',
-      path.resolve(__dirname, `src/api/${apiClient}`)
-    );
-  }
-  // publicPath: process.env.NODE_ENV === 'production' ? '/my-project/' : '/'
+  publicPath:process.env.NODE_ENV === 'production'
+    ? '/cynthiar.github.io/www/'
+    : '/'
 }
